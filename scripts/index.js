2117911bec0c1e52fb914e2be9a1aa4076b26393
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-10-05 09:29:32 -0500

    correct/incorrect + more information reveal for qa

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,19 @@
 //qa
 const pageQuestion = document.getElementById("pages_qa");
 
-const highlightResponse = eventSrc => {
+const highlightResponse = (eventSrc, answer, btnSrc) => {
     document.getElementsByClassName("pages_qa-astro_img")[0].classList.add("hidden");
-    if(btnSrc.dataset.option === correctAnswer) {
+    if(btnSrc.dataset.option === answer) {
         eventSrc.classList.add("correct_answer");
         document.getElementById("correct_answer").classList.remove("hidden");
+        document.querySelector(".correct").classList.remove("hidden");
+
     } else {
         eventSrc.classList.add("incorrect_answer");
         document.getElementById("incorrect_answer").classList.remove("hidden");
+        document.querySelector(".incorrect").classList.remove("hidden");
     }
+    document.querySelector(".more-info").classList.remove("hidden");
 }
 
 if(pageQuestion) {
@@ -21,11 +25,10 @@ if(pageQuestion) {
         const btnSrc = e.submitter;
         const btns = pageQuestion.getElementsByTagName("button");
 
-        //htmlcollection, not array
         for(i = 0; i < btns.length; i++){
             btns[i].disabled = true;
         }
-        highlightResponse(btnSrc);
+        highlightResponse(btnSrc, correctAnswer, btnSrc);
     }
 }
 
