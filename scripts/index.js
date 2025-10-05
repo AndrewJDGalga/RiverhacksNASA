f8fec81553fcc3c8c4bc3cf6fe27fd4c9186b166
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-10-04 23:55:11 -0500

    barebones imageswitching on correct and incorrect guesses.

--- scripts/index.js
+++ scripts/index.js
@@ -7,15 +7,18 @@ pageQuestion.onsubmit = e =>{
     e.preventDefault();
     const btnSrc = e.submitter;
     const btns = pageQuestion.getElementsByTagName("button");
-    
+
     //htmlcollection, not array
     for(i = 0; i < btns.length; i++){
         btns[i].disabled = true;
     }
 
+    document.getElementsByClassName("pages_qa-astro_img")[0].classList.add("hidden");
     if(btnSrc.dataset.option === correctAnswer) {
         btnSrc.classList.add("correct_answer");
+        document.getElementById("correct_answer").classList.remove("hidden");
     } else {
         btnSrc.classList.add("incorrect_answer");
+        document.getElementById("incorrect_answer").classList.remove("hidden");
     }
 }
