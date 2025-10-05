4102ce66757aa1ff25141af8cb199a1bf0be5fa1
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-10-04 23:45:50 -0500

    real basic correct-incorrect answer-freezing quiz functionality.

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,21 @@
 
-//qa blocks
+//qa
+const pageQuestion = document.getElementById("pages_qa");
+const correctAnswer = document.getElementById("pages_qa-question").dataset.answer;
+
+pageQuestion.onsubmit = e =>{
+    e.preventDefault();
+    const btnSrc = e.submitter;
+    const btns = pageQuestion.getElementsByTagName("button");
+    
+    //htmlcollection, not array
+    for(i = 0; i < btns.length; i++){
+        btns[i].disabled = true;
+    }
+
+    if(btnSrc.dataset.option === correctAnswer) {
+        btnSrc.classList.add("correct_answer");
+    } else {
+        btnSrc.classList.add("incorrect_answer");
+    }
+}
