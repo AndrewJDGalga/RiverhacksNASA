2baf63868c85acde56049dd1f9266f8562182f7f
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-10-05 00:34:41 -0500

    JS corrections for errors + menu modifications for more files. Changed files not previously staged for reasons outside understanding.

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 
 //qa
 const pageQuestion = document.getElementById("pages_qa");
-const correctAnswer = document.getElementById("pages_qa-question").dataset.answer;
 
 const highlightResponse = eventSrc => {
     document.getElementsByClassName("pages_qa-astro_img")[0].classList.add("hidden");
@@ -14,17 +13,20 @@ const highlightResponse = eventSrc => {
     }
 }
 
-pageQuestion.onsubmit = e =>{
-    e.preventDefault();
-    const btnSrc = e.submitter;
-    const btns = pageQuestion.getElementsByTagName("button");
+if(pageQuestion) {
+    const correctAnswer = document.getElementById("pages_qa-question").dataset.answer;
 
-    //htmlcollection, not array
-    for(i = 0; i < btns.length; i++){
-        btns[i].disabled = true;
+    pageQuestion.onsubmit = e =>{
+        e.preventDefault();
+        const btnSrc = e.submitter;
+        const btns = pageQuestion.getElementsByTagName("button");
+
+        //htmlcollection, not array
+        for(i = 0; i < btns.length; i++){
+            btns[i].disabled = true;
+        }
+        highlightResponse(btnSrc);
     }
-    highlightResponse(btnSrc);
-    
-    
 }
+
 
